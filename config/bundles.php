06c0a1c17941f2821d7f87daa4cd1b39723b3fdb
ljeandin <luciejeandin@gmail.com>
2024-03-20 22:11:36 +0100

    Make env work

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
